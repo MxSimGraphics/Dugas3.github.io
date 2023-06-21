5d56482d4b5fe2a03a81a3e95617735265c82745
MxSimGraphics <119175064+MxSimGraphics@users.noreply.github.com>
2023-06-21 20:20:16 +0300

    Update select.js

--- kontakti/select.js
+++ kontakti/select.js
@@ -8,14 +8,3 @@ species.forEach(function addSpecies(item) {
     option.value = item;
     slct1.appendChild(option);
 });
-
-let species2 = ["Number plate", "Custom helmet", "Gear", "Seat cover", "Jersey prints", "Gloves",];
-
-let slct2 = document.getElementById("slct2");
-
-species2.forEach(function addSpecies(item) {
-    let option  = document.createElement("option");
-    option.text = item;
-    option.value = item;
-    slct2.appendChild(option);
-});
